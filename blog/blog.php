3c2382e61c192151036eedd4f89ec39a72b62b88
adisat8024 <94816476+adisat8024@users.noreply.github.com>
2024-09-01 22:42:06 +0530

    Add files via upload

---
+++ blog/blog.php
@@ -0,0 +1,126 @@
+<?php
+  $web_name = "Complain Management System";
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <title>CPMS  </title>
+      <link rel="shortcut icon" href="../files/img/ico.ico">
+      <link rel="stylesheet" href="../files/css/bootstrap.css">
+      <link rel="stylesheet" href="../files/css/custom.css">
+</head>
+  <body>
+
+    <nav class="custom_nav">
+        <ul>
+          <li><a href="../index.php">Login</a></li>
+          <li><a href="../signup.php">Signup</a></li>
+          <li><a href="#">Blog</a></li>
+          <li><a href="documentation.php">Documentaion</a></li>
+        </ul>
+    </nav>
+
+  <div class="animated fadeIn">
+    <header class="custom_header">
+        <h1>BLOG</h1>
+        <br><br>
+
+    </header>
+
+    <div class="col-lg-12">
+
+      <div class="container">
+        <br><br>
+        <h1>ARTICLES</h1>
+        <br><br><br>
+        <div class="col-lg-12"><!--row 1-->
+
+          <div class="col-lg-4">
+            <div class="quotes blg text-center">
+              <h3>Tony Snow, Doctor</h3>
+
+                <p>
+                It is important to take care of the pain itself, and it will be followed by the growth of the patient, but at the same time it will happen that there is a lot of work and pain. For to come to the smallest detail, no one should practice any kind of work unless he derives some benefit from it.</p>
+
+                <br>
+                <a href="#" class="read">Read more</a>
+                <br><br><br><br>
+            </div>
+
+          </div>
+
+          <div class="col-lg-4">
+            <div class="quotes blg">
+              <h3>Christina Alen, Artist</h3>
+
+                <p>Art is not what you see, but what you make others see,capturing the essence of creativity. Every artist was first an amateur emphasizing the journey of growth and learning. Creativity takes courage, acknowledging the bravery required to express oneself.</p>
+                <a href="#" class="read">Read more</a>
+                <br><br><br><br>
+            </div>
+          </div>
+
+          <div class="col-lg-4">
+            <div class="quotes blg">
+              <h3>Ronnie Tyler, Singer</h3>
+                <p>Music can change the world because it can change people . "Where words fail, music speaks," underscoring the unique ability of music to convey emotions. Singing was an escape to another world, where she was no longer bound by earthly limitations.</p>
+                <a href="#" class="read">Read more</a>
+                <br><br><br><br>
+            </div>
+          </div>
+
+
+        </div>
+
+        <br><br><br>
+        <div class="col-lg-12"><!--row 2-->
+
+          <div class="col-lg-4">
+            <div class="quotes blg">
+              <h3>Tory Tyler, Politician</h3>
+                <p>The best way to predict your future is to create it urging leaders to take proactive steps towards change. "Politics is the art of the possible," remind us of the importance of pragmatism and compromise.A politician thinks of the next election. A statesman, of the next generation.</p>
+                <a href="#" class="read">Read more</a>
+                <br><br><br><br>
+            </div>
+
+          </div>
+
+
+            <div class="col-lg-4">
+              <div class="quotes blg">
+                <h3>Ronnie Tyler, Singer</h3>
+                  <p>Music can change the world because it can change people . "Where words fail, music speaks," underscoring the unique ability of music to convey emotions. Singing was an escape to another world, where she was no longer bound by earthly limitations.</p>
+                  <a href="#" class="read">Read more</a>
+                  <br><br><br><br>
+              </div>
+            </div>
+
+
+          <div class="col-lg-4">
+            <div class="quotes blg">
+                <p>Believe you can and you're halfway there,reminding us of the power of self-belief. "Success is not final, failure is not fatal: It is the courage to continue that counts," emphasizing the importance of perseverance. "The only limit to our realization of tomorrow is our doubts of today," ,urging us to overcome self-doubt.</p>
+                <a href="#" class="read">Read more</a>
+                <br><br><br><br>
+            </div>
+          </div>
+        </div>
+
+
+
+      </div>
+    </div>
+  </div>
+
+    <footer>
+    <br><br>&copy <?php echo date("Y"); ?> <?php echo $web_name; ?>
+    </footer>
+
+    <script src="../files/js/jquery.js"></script>
+    <script src="../files/js/bootstrap.min.js"></script>
+    <script src="../files/js/script.js"></script>
+
+  </body>
+</html>
